4bc70a55f7a019936be4ee123f906668150e4587
juntaozhou05 <350529307@qq.com>
2018-04-13 13:50:33 +0800

    update

--- src/pages/AutoPlay/index.js
+++ src/pages/AutoPlay/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Carousel, WingBlank } from "antd-mobile";
+import { Carousel } from "antd-mobile";
 
 class AutoPlay extends Component {
   constructor(props) {
@@ -28,11 +28,11 @@ class AutoPlay extends Component {
         <Carousel
           autoplay={true}
           infinite
-          selectedIndex={1}
-          beforeChange={(from, to) =>
-            console.log(`slide from ${from} to ${to}`)
-          }
-          afterChange={index => console.log("slide to", index)}
+          selectedIndex={0}
+          // beforeChange={(from, to) =>
+          //   console.log(`slide from ${from} to ${to}`)
+          // }
+          // afterChange={index => console.log("slide to", index)}
         >
           {this.state.data.map(val => (
             <a
