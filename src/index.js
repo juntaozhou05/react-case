d47f16afd5498fdf94796dccb1a31cf8f3938a97
zhoujuntao <350529307@qq.com>
2019-05-19 12:43:08 +0800

    webapck test

--- src/index.js
+++ src/index.js
@@ -1,19 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import "./index.css";
-import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
-import { Provider } from "mobx-react";
-import Store from "./Store";
-const store = new Store();
+import _ from "lodash";
+import "./style/index.css";
 
-ReactDOM.render(
-  <BrowserRouter>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </BrowserRouter>,
-  document.getElementById("root")
-);
-registerServiceWorker();
+function createDomElement() {
+  let dom = document.createElement("div");
+  dom.innerHTML = _.join(["aicoder.com", "hao", "xuexi"]);
+  return dom;
+}
+let divDom = createDomElement();
+document.body.appendChild(divDom);
