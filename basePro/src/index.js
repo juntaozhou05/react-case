14770082c6cf4795f00d6e92bd86b3ccfa87079f
zhoujuntao <zhoujuntao@zhoujuntaodeMacBook-Pro.local>
2019-07-05 15:11:43 +0800

    webpack基本配置加热更新

--- basePro/src/index.js
+++ basePro/src/index.js
@@ -1,6 +1,28 @@
-import "./style.css"
-import "./style.less"
+import printMe from './print.js';
+import './style.css';
 
-let div = document.createElement('div')
-div.innerHTML = "new div"
-document.body.appendChild(div)
+  function component() {
+    var element = document.createElement('div');
+    var btn = document.createElement('button');
+
+    element.innerHTML = "abcde12"
+
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;  // onclick event is bind to the original printMe function
+
+    element.appendChild(btn);
+
+    return element;
+  }
+
+  let element = component();
+  document.body.appendChild(element);
+
+  if (module.hot) {
+    module.hot.accept('./print.js', function() {
+      console.log('Accepting the updated printMe module!');
+      document.body.removeChild(element);
+      element = component(); // Re-render the "component" to update the click handler
+      document.body.appendChild(element);
+    })
+  }
--- basePro/webpack.config.js
+++ basePro/webpack.config.js
@@ -2,32 +2,66 @@ const path = require('path');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const devMode = process.env.NODE_ENV !== 'production'
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const {CleanWebpackPlugin} = require("clean-webpack-plugin")
+const webpack = require('webpack');
 
 module.exports = {
   mode:"development",
-  entry: './src/index.js',
+  entry: {
+    app:'./src/index.js',
+  },
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, './dist')
+    filename: '[name].js',
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/'
   },
   module: {
     rules: [
       {
         test: /\.(le|c)ss$/,
         use: [
-           MiniCssExtractPlugin.loader,
-           'css-loader',
-           'less-loader'
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: true,
+            },
+          },
+          'css-loader',
+          'less-loader'
         ],
-
       },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test:/\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader'
+        ]
+      }
     ]
   },
+  devtool:"inline-source-map",
+  devServer:{
+    contentBase:'./dist',
+    hot:true
+  },
   plugins:[
-      new MiniCssExtractPlugin({
-          filename:'[name].[hash].css',
-          chunkFilename:'[id].[hash].css'
-      })
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title:"Output management",
+      template:'src/index.html'
+    }),
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new MiniCssExtractPlugin({
+        filename:'[name].css',
+        chunkFilename:'[id].css'
+    })
   ],
   optimization: {
     minimizer: [new OptimizeCSSAssetsPlugin({})]
